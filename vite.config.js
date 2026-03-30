3be6da7711c3295134284c0e079728d2afd1d0e9
Peter <montalbancastro.p@gmail.com>
2026-03-30 13:52:39 -0300

    Configuración de base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // REEMPLAZA 'TU-NOMBRE-DE-REPOSITORIO' por el nombre exacto de tu repo en GitHub
+  base: '/Portafolio-actualizadito/', 
   plugins: [
     vue(),
     vueDevTools(),
